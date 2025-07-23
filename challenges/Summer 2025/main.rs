046afeb65f7203e0a3c5ddb33b365e92742fc9b4
tavio <github.qhgby@passmail.net>
2025-07-23 12:40:54 +0200

    backup, need to test something

--- challenges/Summer 2025/main.rs
+++ challenges/Summer 2025/main.rs
@@ -61,11 +61,11 @@ fn update_agent(agents: &mut Vec<Agent>, id: i32, x: i32, y: i32, cooldown: i32,
     agents[i].alive = true;
 }
 
-fn get_dist(agent_1: &Agent, agent_2: &Agent) -> i32
+fn get_dist(pos_a: &Coords, pos_b: &Coords) -> i32
 {
     return
-    (cmp::max(agent_1.pos.x, agent_2.pos.x) - cmp::min(agent_1.pos.x, agent_2.pos.x))
-    + (cmp::max(agent_1.pos.y, agent_2.pos.y) - cmp::min(agent_1.pos.y, agent_2.pos.y))
+    (cmp::max(pos_a.x, pos_b.x) - cmp::min(pos_a.x, pos_b.x))
+    + (cmp::max(pos_a.y, pos_b.y) - cmp::min(pos_a.y, pos_b.y))
 }
 
 fn get_closest(agents: & Vec<Agent> , ag: &Agent) -> i32
@@ -79,7 +79,7 @@ fn get_closest(agents: & Vec<Agent> , ag: &Agent) -> i32
         {    
             continue
         }
-        let tmp = get_dist(ag, &agents[i]);
+        let tmp = get_dist(&ag.pos, &agents[i].pos);
         if tmp < dist
         {
             dist = tmp;
@@ -127,7 +127,7 @@ fn main() {
             let x = parse_input!(inputs[3*j], i32); // X coordinate, 0 is left edge
             let y = parse_input!(inputs[3*j+1], i32); // Y coordinate, 0 is top edge
             let tile_type = parse_input!(inputs[3*j+2], i32);
-            grid.extend([Tile{pos: Coords {x: -1, y: -1}, cover: tile_type,}]);
+            grid.extend([Tile{pos: Coords {x: x, y: y}, cover: tile_type,}]);
         }
     }
 
@@ -159,35 +159,24 @@ fn main() {
             //eprintln!("agent[{}]: {:?}", j, agents[j]);
             if agents[j].player == my_id
             {
-                let close = get_closest(&agents, &agents[j]);
-                let tmp: Vec<_> = agents.iter().filter(|t| t.id == close).cloned().collect();
-                eprintln!("tmp: {:?}", tmp[0]);
-                if get_dist(&agents[j], &tmp[0]) <= 2
-                {
-                    println!("SHOOT {}", tmp[0].id);
-                    continue
-                }
-                if get_dist(&agents[j], &tmp[0]) <= agents[j].optimal_range
-                {
-                    println!("THROW {} {}", tmp[0].pos.x, tmp[0].pos.y);
-                    continue
-                }
-                else
-                {
-                    println!("MOVE {} {}", tmp[0].pos.x, tmp[0].pos.y);
-                    continue
-                }
                 let mut aim = Coords {x: -1, y: -1,};
                 let mut aim_value: i32 = -1;
-                for i in 0..grid.len()
+                for i in 1..grid.len()
                 {
-                    let mut temp: i32 = 0;
+                    let mut temp: i32 = -1;
                     for x in -1..1
                     {
                         for y in -1..1
                         {
-                            let t: Vec<_> = agents.iter().filter(|t| t.pos.x == x && t.pos.y == y).cloned().collect();
+                            if grid[i].pos.x + x < 0 || grid[i].pos.y + y < 0 || grid[i].pos.x + x >= width || grid[i].pos.y + y >= height
+                            {
+                                //eprintln!("stop {} {} | {} {} | {}", grid[i].pos.x, grid[i].pos.y, x, y, i);
+                                continue
+                            }
+                            let t: Vec<_> = agents.iter().filter(|t| t.pos.x == grid[i].pos.x + x && t.pos.y == grid[i].pos.y + y)
+                                .cloned().collect();
                             temp += t.len() as i32;
+                            //eprintln!("{}:{} {}", x, y, t.len());
                             if t.len() > 0 && t[0].player == my_id
                             {
                                 temp -= 10;
@@ -196,13 +185,94 @@ fn main() {
                     }
                     if temp > aim_value
                     {
-                        aim = grid[i].pos;
+                        aim.x = grid[i].pos.x;
+                        aim.y = grid[i].pos.y;
                         aim_value = temp;
+                        eprintln!("verif aim {}:{}", aim.x, aim.y);
+                    }
+                }
+                eprintln!("{}: {:?}", agents[j].id, aim);
+                if get_dist(&agents[j].pos, &aim) <= 2
+                {
+                    let t: Vec<_> = agents.iter().filter(|t| t.pos.x == agents[j].pos.x && t.pos.y == agents[j].pos.x).cloned().collect();
+                    println!("HUNKER_DOWN");
+                    //println!("SHOOT {}", t[0].id);
+                }
+                else if get_dist(&agents[j].pos, &aim) <= 4 && agents[j].splash_bombs > 0
+                {
+                    println!("THROW {} {}", aim.x, aim.y);
+                }
+                else if agents[j].splash_bombs > 0
+                {
+                    let tmp_x = aim.x;
+                    let tmp_y = aim.y;
+                    while get_dist(&agents[j].pos, &aim) > 4
+                    {
+                        eprintln!("debug 2, {}:{}", aim.x, aim.y);
+                        if (agents[j].pos.x - aim.x).abs() > (agents[j].pos.y - aim.y).abs()
+                        {
+                            if agents[j].pos.x < aim.x
+                            {
+                                aim.x -= 1;
+                            }
+                            else
+                            {
+                                aim.x += 1;
+                            }
+                        }
+                        else
+                        {
+                            if agents[j].pos.y < aim.y
+                            {
+                                aim.y -= 1;
+                            }
+                            else
+                            {
+                                aim.y += 1;
+                            }
+                        }
+                    }
+                    println!("MOVE {} {}; THROW {} {}", tmp_x, tmp_y, aim.x, aim.y);
+                }
+                else
+                {
+                    let close = get_closest(&agents, &agents[j]);
+                    let t: Vec<_> = agents.iter().filter(|t| t.id == close).cloned().collect();
+                    if get_dist(&agents[j].pos, &t[0].pos) <= 2
+                    {
+                        //println!("SHOOT {}", t[0].id);
+                        println!("HUNKER_DOWN");
+                        continue
+                    }
+                    else if get_dist(&agents[j].pos, &t[0].pos) <= agents[j].optimal_range
+                    {
+                        println!("THROW {} {}", t[0].pos.x, t[0].pos.y);
+                        continue
+                    }
+                    else
+                    {
+                        println!("MOVE {} {}", t[0].pos.x, t[0].pos.y);
+                        continue
                     }
                 }
             }
         }
-//        println!("MOVE 10 3; THROW 10 3");
-  //      println!("MOVE 1 10; THROW 2 8; MESSAGE let's die");
     }
 }
+
+   //             eprintln!("tmp: {:?}", tmp[0]);
+    //            if get_dist(&agents[j], &tmp[0]) <= 2
+     //           {
+      //              println!("SHOOT {}", tmp[0].id);
+       //             continue
+        //        }
+         //       if get_dist(&agents[j], &tmp[0]) <= agents[j].optimal_range
+          //      {
+           //         println!("THROW {} {}", tmp[0].pos.x, tmp[0].pos.y);
+            //        continue
+             //   }
+              //  else
+               // {
+                //    println!("MOVE {} {}", tmp[0].pos.x, tmp[0].pos.y);
+                 //   continue
+                //}
